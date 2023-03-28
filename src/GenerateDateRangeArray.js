cf2543d5deab86352371fe70eb1e96240ae77d43
bronifty <ybronifty@gmail.com>
2023-03-28 07:40:04 +0000

    adding a date range generator for last n days which we will use to loop over the folder date and snag the files

---
+++ src/GenerateDateRangeArray.js
@@ -0,0 +1,45 @@
+// function generateDateRangeArray(n) {
+//   const days = [];
+//   const today = new Date();
+
+//   for (let i = 0; i < n; i++) {
+//     const currentDate = new Date(today);
+//     currentDate.setDate(currentDate.getDate() - i);
+
+//     const year = currentDate.getFullYear();
+//     const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+//     const day = String(currentDate.getDate()).padStart(2, '0');
+
+//     const formattedDate = `${year}${month}${day}`;
+//     days.push(formattedDate);
+//   }
+
+//   return days;
+// }
+
+function generateDateRangeArray(n) {
+  const days = [];
+  const today = new Date().toLocaleString("en-US", {
+    timeZone: "America/New_York",
+  });
+
+  for (let i = 0; i < n; i++) {
+    const currentDate = new Date(today);
+    currentDate.setDate(currentDate.getDate() - i);
+
+    const year = currentDate.getUTCFullYear();
+    const month = String(currentDate.getUTCMonth() + 1).padStart(2, "0");
+    const day = String(currentDate.getUTCDate()).padStart(2, "0");
+
+    const formattedDate = `${year}${month}${day}`;
+
+    days.push(formattedDate);
+  }
+
+  return days;
+}
+
+module.exports = generateDateRangeArray;
+
+// const dateRange = generateDateRangeArray(7);
+// console.log(dateRange);
